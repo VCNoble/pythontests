4fb7d1dea36765ef37df1253cf8e21e886ffcb3e
VCNoble <112306925+VCNoble@users.noreply.github.com>
2022-08-31 13:06:56 +1200

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -2,11 +2,11 @@ import os
 
 
 def main ():
-  print("Hello from GitHub Actions!")
+  print("Hello from Turners!")
   token = os.environ.get("GOOGLE_APIKEY")
   if not token:
     raise RuntimeError("GOOGLE_APIKEY env var is not set!")
-  print("All good! we found Turners env var")
+  print("Passed! we found Turners env var")
   
 if __name__=='__main__':
   main()
